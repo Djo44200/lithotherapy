081d90f2547c9ce0d9a826d3faf913dde084f83e
Jonathan <jonathan.bodet2019@campus-eni.fr>
2020-01-22 17:48:42 +0100

    Mise en place de la connexion et redirection page principal

--- src/Controller/PrincipalController.php
+++ src/Controller/PrincipalController.php
@@ -1,1 +1,28 @@
 <?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PrincipalController extends Controller
+{
+    /**
+     * @Route("/index",name="app_Index")
+     */
+    public function default()
+    {
+        if ($this->getUser()){
+            // Récupération de l'ID du user en session
+            $userId = $this->get('security.token_storage')->getToken()->getUser()->getId();
+            return $this->render('principal/principal.html.twig', [
+                'user' => $userId
+            ]);
+
+        }
+
+        return $this->redirectToRoute('app_login');
+
+
+    }
+}
--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -94,7 +94,7 @@ class UserAuthenticator extends AbstractFormLoginAuthenticator
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        return new RedirectResponse($this->urlGenerator->generate('sortie_index'));
+        return new RedirectResponse($this->urlGenerator->generate('app_Index'));
         throw new Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
